92a4de31173354ed0062299fc11c992ba2f8f92e
ppatch <605351841@qq.com>
2015-08-12 16:54:18 +0800

    litter change

--- www/lib/ionic/js/ionic.bundle.js
+++ www/lib/ionic/js/ionic.bundle.js
@@ -52985,7 +52985,7 @@ IonicModule
       $scope.$on('$ionicView.beforeEnter', function(ev, viewData) {
         if (viewData.enableBack) {
           var sideMenuCtrl = $element.inheritedData('$ionSideMenusController');
-          if (!sideMenuCtrl.enableMenuWithBackViews()) {
+          if (sideMenuCtrl & !sideMenuCtrl.enableMenuWithBackViews()) {
             $element.addClass('hide');
           }
         } else {
